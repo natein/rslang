24afb4445534d6560b73ed24e163f5c21c8d0695
Alexandr <k.v.alexandr@gmail.com>
2021-03-25 16:37:15 +0400

    refactor: user action and reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,11 +1,13 @@
+import { USER, USER_LOGOUT } from "../actions/userActions";
+
 const initialState = JSON.parse(localStorage.getItem('rslang-team15-user')) || {};
 
 const userReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'USER': {
+        case USER: {
             return { ...state, ...action.payload };
         }
-        case 'USER_LOGOUT': {
+        case USER_LOGOUT: {
             return {};
         }
         default: {
