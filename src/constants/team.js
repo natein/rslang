7e40e950c9404ce2f72d350975b5df6a4c44891a
Natein <natein@gmail.com>
2021-03-25 04:22:52 +0200

    feat: add team data

---
+++ src/constants/team.js
@@ -0,0 +1,68 @@
+import kvalexandr from '../assets/team/kvalexandr.jpg';
+import aleksei from '../assets/team/aleksei-bulgak-study.jpg';
+import kvadeck from '../assets/team/kvadeck.jpg';
+import spaceragga from '../assets/team/spaceragga.jpg';
+import natein from '../assets/team/natein.jpg';
+
+
+const team = [
+  {
+    name: 'Aliaksei Bulhak',
+    github: 'aleksei-bulgak-study',
+    photo: aleksei,
+    role: 'Developer',
+    tasks: [
+      'It is a long established fact',
+      'when an unknown printer',
+      'release of Letraset'
+    ],
+  },
+  {
+    name: 'Alexandr Kudryavtsev',
+    github: 'kvalexandr',
+    photo: kvalexandr,
+    role: 'Developer',
+    tasks: [
+      'the leap into electronic',
+      'In the 1960s with',
+      'Aldus PageMaker including',
+      'Where does it come from?'
+    ],
+  },
+  {
+    name: 'Nikolay Volzhenin',
+    github: 'Kvadeck',
+    photo: kvadeck,
+    role: 'Developer',
+    tasks: [
+      'It is a long established fact',
+      'when an unknown printer',
+      'release of Letraset'
+    ],
+  },
+  {
+    name: 'Ivan Mikhalchanka',
+    github: 'spaceragga',
+    photo: spaceragga,
+    role: 'Developer',
+    tasks: [
+      'the leap into electronic',
+      'In the 1960s with',
+      'Aldus PageMaker including',
+      'Where does it come from?'
+    ],
+  },
+  {
+    name: 'Natalija Eina',
+    github: 'natein',
+    photo: natein,
+    role: 'Developer',
+    tasks: [
+      'It is a long established fact',
+      'when an unknown printer',
+      'release of Letraset'
+    ],
+  },
+];
+
+export default team;
