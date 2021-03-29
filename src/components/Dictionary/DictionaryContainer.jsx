3f4efaf6ddaf98649f2c711eb9ff6888a3d00ff3
Alexandr <k.v.alexandr@gmail.com>
2021-03-29 19:36:58 +0400

    refactor: dictionary

--- src/components/Dictionary/DictionaryContainer.jsx
+++ src/components/Dictionary/DictionaryContainer.jsx
@@ -1,24 +1,20 @@
 import { useEffect, useCallback, useRef } from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router';
-import { loadWords, createUserWord, updateUserWord, loadUserWordAgregate } from '../../actions/ebookActions';
+import { loadWords, loadUserWordAgregate } from '../../actions/ebookActions';
 import Words from '../Words';
 import LoadingPage from '../LoadingPage';
 import Snackbar from '@material-ui/core/Snackbar';
 import DictionaryGroupMenu from '../WordsPanel/DictionaryGroupMenu';
-import EbookPageMenu from '../WordsPanel/EbookPageMenu';
 import GameMenu from '../WordsPanel/GameMenu';
 import Settings from '../WordsPanel/Settings';
 
 function DictionaryContainer(props) {
   const {
     loader,
-    loadWords,
     page,
     group,
     user,
-    createUserWord,
-    updateUserWord,
     error,
     loadUserWordAgregate,
     type
@@ -36,29 +32,13 @@ function DictionaryContainer(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [type, page]);
 
-  const onChangeDifficulty = (word, type) => {
-    if (word?.userWord) {
-      updateUserWord(user.userId, word._id, { difficulty: type }, user.token);
-    } else {
-      createUserWord(user.userId, word._id, { difficulty: type }, user.token);
-    }
-  }
-
-  const onDeleteWord = (word) => {
-    if (word?.userWord) {
-      updateUserWord(user.userId, word._id, { optional: { isDelete: true } }, user.token);
-    } else {
-      createUserWord(user.userId, word._id, { optional: { isDelete: true } }, user.token);
-    }
-  }
-
   return (
     <>
       {loader && <LoadingPage />}
       {<DictionaryGroupMenu {...props} routeTypePage={routeTypePage} />}
       {<GameMenu />}
       {<Settings />}
-      {!loader && <Words {...props} onChangeDifficulty={onChangeDifficulty} onDeleteWord={onDeleteWord} audio={audio} dictionary={true} />}
+      {!loader && <Words {...props} audio={audio} dictionary={true} />}
 
       {error && <Snackbar open={true} message={error} />}
     </>
@@ -77,8 +57,6 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     loadWords: (page, group) => dispatch(loadWords(page, group)),
-    createUserWord: (userId, wordId, word, token) => dispatch(createUserWord(userId, wordId, word, token)),
-    updateUserWord: (userId, wordId, word, token) => dispatch(updateUserWord(userId, wordId, word, token)),
     loadUserWordAgregate: (userId, token, group, page, isHard, isDelete, type) => dispatch(loadUserWordAgregate(userId, token, group, page, isHard, isDelete, type)),
   }
 }
