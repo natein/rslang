a06d994523cc8d1824c3dcba4aa86265337e72f7
aliaksei bulhak <aliaksei.bulhak@nike.com>
2021-04-01 18:28:09 +0300

    fix: complexity level selection for sprint game

--- src/components/SprintGame/SelectComplexityLevel.jsx
+++ src/components/SprintGame/SelectComplexityLevel.jsx
@@ -68,22 +68,22 @@ const SelectComplexityLevel = ({ onLoadWords }) => {
                     label="Сложности"
                     className={classes.select}
                 >
-                    <MenuItem key={0} value={0}>
+                    <MenuItem key={1} value={1}>
                         1
                     </MenuItem>
-                    <MenuItem key={1} value={1}>
+                    <MenuItem key={2} value={2}>
                         2
                     </MenuItem>
-                    <MenuItem key={2} value={2}>
+                    <MenuItem key={3} value={3}>
                         3
                     </MenuItem>
-                    <MenuItem key={3} value={3}>
+                    <MenuItem key={4} value={4}>
                         4
                     </MenuItem>
-                    <MenuItem key={4} value={4}>
+                    <MenuItem key={5} value={5}>
                         5
                     </MenuItem>
-                    <MenuItem key={5} value={5}>
+                    <MenuItem key={6} value={6}>
                         6
                     </MenuItem>
                 </Select>
