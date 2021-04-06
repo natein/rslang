495b7fd62d91a071de73d9770bf3e6b53842b576
aliaksei bulhak <aliaksei.bulhak@nike.com>
2021-04-06 09:56:48 +0300

    feat: add day statistics by game

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -23,7 +23,7 @@ function Statistics({ statistics, loadStatistics, clearStatistics }) {
             <Grid container spacing={3}>
                 <Grid item xs={12} md={8} lg={9}>
                     <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', height: 240 }}>
-                        <DayChart />
+                        <DayChart data={statistics?.optional?.gameStatistics}/>
                     </Paper>
                 </Grid>
                 <Grid item xs={12} md={4} lg={3}>
